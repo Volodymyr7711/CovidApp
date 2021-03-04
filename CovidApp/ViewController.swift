e5994ca4e75a32ee7467229509195ddc95e1fc16
Volodymyr7711 <volodumurbodnarchuk70@gmail.com>
2021-03-04 22:08:31 +0200

    Initial Commit

---
+++ CovidApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CovidApp
+//
+//  Created by Volodymyr Mendyk on 04.03.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
